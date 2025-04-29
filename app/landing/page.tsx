27e8aaecb75daabce083f787fbfd147918ec9ad6
CSchwaebe <channingschwaebe@gmail.com>
2025-04-28 21:22:58 -0700

    Update page.tsx

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -62,14 +62,14 @@ export default function LandingPage() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
-            className="text-center relative w-full max-w-md h-16 md:h-24 xl:h-32 2xl:h-40 pb-2 md:pb-8 xl:pb-16 2xl:pb-32 flex items-center justify-center"
+            className="text-center relative w-full max-w-md h-12 sm:h-16 md:h-24 xl:h-32 2xl:h-40 pb-2 md:pb-8 xl:pb-16 2xl:pb-32 flex items-center justify-center"
           >
             <Link href="/" className="block group w-full h-full">
               <div className="relative transform transition-transform duration-300 group-hover:scale-105 h-full flex items-center justify-center">
                 <div className="absolute -top-px left-0 right-0 h-px bg-gradient-to-r from-transparent via-foreground to-transparent opacity-50 group-hover:opacity-100 transition-opacity duration-300"></div>
                 <div className="absolute -bottom-px left-0 right-0 h-px bg-gradient-to-r from-transparent via-foreground to-transparent opacity-50 group-hover:opacity-100 transition-opacity duration-300"></div>
-                <div className="relative py-4 md:py-6 xl:py-12 2xl:py-32 px-4">
-                  <h2 className={`text-2xl md:text-4xl xl:text-5xl 2xl:text-7xl font-bold text-foreground bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground to-foreground animate-text-shimmer ${iceland.className}`}>
+                <div className="relative py-4 sm:py-4 md:py-6 xl:py-12 2xl:py-32 px-4">
+                  <h2 className={`text-xl sm:text-2xl md:text-4xl xl:text-5xl 2xl:text-7xl font-bold text-foreground bg-clip-text text-transparent bg-gradient-to-r from-foreground via-foreground to-foreground animate-text-shimmer ${iceland.className}`}>
                     Enter Game
                   </h2>
                 </div>
